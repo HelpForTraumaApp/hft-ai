abc29bcd3844e4f0a42b2467ee0b36b818aeabce
CesarCanas1206 <cesarcanas1993@gmail.com>
2024-09-24 19:06:58 -0600

    fix deploy error on dialogue

--- app/dialogue/page.tsx
+++ app/dialogue/page.tsx
@@ -1,9 +1,9 @@
 import { auth } from '@/auth'
 import { Session } from '@/lib/types'
-import { Dialogue } from '@/components/dialogue/Dialogue'
+import { Dialogue } from '@/components/dialogue/dialogue'
 import { redirect } from 'next/navigation'
 
-export default async function DashboardPage() {
+export default async function DialoguePage() {
   const session = (await auth()) as Session
 
   if (!session) {
