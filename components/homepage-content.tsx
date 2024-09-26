8daed89874fd25179babac2334ff88f00aca4d63
DarkoArnautov <darkoservantes@outlook.com>
2024-09-26 13:16:12 +0100

    add color modal on the stories page

--- components/homepage-content.tsx
+++ components/homepage-content.tsx
@@ -10,8 +10,8 @@ interface ContentProps {
 const Content = ({ id, title, description, children }: ContentProps) => {
   return (
     <div id={id} className="homepage-content py-5">
-      <h1 className="font-bold text-[24px]">{title}</h1>
-      <p className="font-bold text-[16px] my-2">{description}</p>
+      <h1 className="font-bold text-2xl">{title}</h1>
+      <p className="font-bold text-base my-2">{description}</p>
       {children}
     </div>
   )
--- components/stories/stories.tsx
+++ components/stories/stories.tsx
@@ -1,36 +1,31 @@
 'use client'
 import RGB_Color_Icont from '@/app/assets/icons/rgb_color_icon.svg'
 import Image from 'next/image'
-import { ColorModal } from '../modals/colormodal'
+import { ColorModal } from '@/components/modals/colorModal'
 import { useState } from 'react'
-import { AddEventModal } from '../modals/addeventmodal'
+import { AddEventModal, colorData } from '@/components//modals/addEventModal'
+
+export type EventInfoType = {
+  eventName: string
+  ageYears: number
+  ageMonths: number
+  eventDescription: string
+  selectedColor: string
+}
+
 export const Stories = () => {
   const [colorModalOpened, setColorModalOpened] = useState(0)
   const [eventModalOpened, setEventModalOpened] = useState(0)
-  const [selectedColor, setSelectedColor] = useState('')
+  const [selectedColorForSort, setSelectedColorForSort] = useState('')
+  const [eventDetailInfo, setEventDetailInfo] = useState<EventInfoType | null>(
+    null
+  )
 
   const handleSortByColor = (index: number) => {
-    console.log('aaa', index)
+    setSelectedColorForSort(colorData[index])
   }
 
-  const colorData: string[] = [
-    '#343032',
-    '#A7A9AC',
-    '#5365FA',
-    '#FCF31C',
-    '#FC3F50',
-    '#69C6F7',
-    '#9AD39B',
-    '#38795F',
-    '#5A41A8',
-    '#FF874F',
-    '#FF874F',
-    '#ED95C9',
-    '#F1E2D4',
-    '#E8DAC0',
-    '#C6A889',
-    '#976F5B'
-  ]
+  const addEvent = (eventInfo: EventInfoType) => {}
 
   return (
     <div className="relative mx-auto px-12">
@@ -40,35 +35,40 @@ export const Stories = () => {
       <div className="pt-2 flex justify-start">
         <button
           onClick={() => setColorModalOpened(1)}
-          className="bg-white flex justify-around px-4 py-2 rounded-[6px] border-[#e4e4e4]  cursor-pointer shadow-[0_1px_1px_0px_rgba(0,0,0,0.5)] hover:bg-[#f7f7f7] "
+          className="bg-white flex justify-around px-4 py-2 rounded cursor-pointer shadow-[0_1px_1px_0px_rgba(0,0,0,0.5)] hover:bg-[#f7f7f7]"
         >
           <Image
             className="mr-2"
             width={24}
             height={24}
             src={RGB_Color_Icont}
-            alt="rgb_color_icon"
+            alt="rgb color icon"
           />
           Color
         </button>
       </div>
-      <div className="text-md text-center   ">
+      <div className="text-md text-center">
         <button
           onClick={() => setEventModalOpened(1)}
-          className="py-4 px-3 mt-2 tracking-[2px] rounded-[6px] leading-[1.5px] border-[#16c1fb] bg-[#16c1fb] text-white hover:bg-[#04ade7]"
+          className="py-4 px-3 mt-2 tracking-[2px] rounded-[6px] leading-[1.5px] bg-[#16c1fb] text-white hover:bg-[#04ade7]"
         >
           ADD EVENT
         </button>
       </div>
       {colorModalOpened > 0 && (
-        <ColorModal
-          onClose={() => setColorModalOpened(0)}
-          handleSortByColor={handleSortByColor}
-          colorData={colorData}
-        />
+        <div className="absolute w-[232px] z-2 p-4 bg-white rounded-md top-16 shadow-[0 47px 74px rgba(0,0,0,.06),0 9.4px 12.025px rgba(0,0,0,.12)]">
+          <ColorModal
+            onClose={() => setColorModalOpened(0)}
+            selectColor={handleSortByColor}
+            colorData={colorData}
+          />
+        </div>
       )}
       {eventModalOpened > 0 && (
-        <AddEventModal onClose={() => setColorModalOpened(0)} />
+        <AddEventModal
+          onClose={() => setEventModalOpened(0)}
+          handleAddEvent={addEvent}
+        />
       )}
     </div>
   )
