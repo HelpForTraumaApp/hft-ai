940e6463545d3fd61a4de3f5b84c5d4186942fee
luisjose1996 <luisnavedag10@gmail.com>
2024-10-02 16:45:51 -0500

    fixed type error

--- app/api/place/route.ts
+++ app/api/place/route.ts
@@ -2,8 +2,15 @@ import { NextResponse } from 'next/server';
 import { getAuth } from '@/lib/auth';
 import { createPlace, updatePlace, selectAllPlaces } from '@/lib/actions/places'
 
+
 export async function GET(req: Request) {
     const user_id = await getAuth();
+
+    // Check if user_id is null or empty
+    if (!user_id) {
+        return NextResponse.json({ error: 'User not authenticated' }, { status: 401 });
+    }
+
     const result = await selectAllPlaces(user_id);
     return NextResponse.json({ result });
 }
